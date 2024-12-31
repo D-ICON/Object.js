cb6a88b5715fe78b9cec0d4dfc20b89aaf371403
D-ICON <delawadetunji@gmail.com>
2024-12-31 13:27:05 +0100

    first commit

--- Object/objectTasks.js
+++ Object/objectTasks.js
@@ -1,24 +1,6 @@
-// const books = {
-//     title: "take me home",
-//     description: "is about a girl",
-//     numberOfPages: 218,
-//     author: "adewale",
-//     reading: true
-// }
-// console.log(books)
-const books = {
-    title: "take me home",
-    description: "is about a girl",
-    numberOfPages: 218,
-    author: "adewale",
-    reading: false,
-    toggleReadingStatus: function(){
-        if (books.reading ===true)
-            books.reading =false
-            else {
-                books.reading = true
-            }
-    }
-}
- books.toggleReadingStatus()
-console.log(books.reading)
+const area = (5 * 7)
+
+console.log(area)
+
+
+
